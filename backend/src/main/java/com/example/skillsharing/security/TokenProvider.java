0ceb6d5bee553b4cef62be7ef265e3e58d460792
VINUKA-DINETHMIN <IT22248626@my.sliit.lk>
2025-03-15 17:51:52 +0530

    Add backend/src/main/java/com/example/skillsharing/security/TokenProvider.java 1742041312694

---
+++ backend/src/main/java/com/example/skillsharing/security/TokenProvider.java
@@ -0,0 +1,74 @@
+package com.example.skillsharing.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Service
+public class TokenProvider {
+
+    @Value("${app.auth.tokenSecret}")
+    private String tokenSecret;
+
+    @Value("${app.auth.tokenExpirationMsec}")
+    private long tokenExpirationMsec;
+
+    private final Key key;
+
+    public TokenProvider(@Value("${app.auth.tokenSecret}") String tokenSecret) {
+        this.key = Keys.hmacShaKeyFor(tokenSecret.getBytes());
+    }
+
+    public String createToken(Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + tokenExpirationMsec);
+
+        return Jwts.builder()
+                .setSubject(Long.toString(userPrincipal.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty");
+        }
+        return false;
+    }
+}
